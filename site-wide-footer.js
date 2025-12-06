2fca8e0099505282d31b874b1a081a2257e903c7
edwardvaneechoud <evaneechoud@gmail.com>
2025-12-06 10:31:27 +0100

    remove redirect

--- site-wide-footer.js
+++ site-wide-footer.js
@@ -1319,29 +1319,7 @@ window.addEventListener('load', function() {
   });
 })();
 
-(function() {
-  const redirectMap = {
-    '/profile': '/account#profile',
-    '/profile.html': '/account#profile',
-    '/my-rentals': '/account#rentals',
-    '/my-rentals.html': '/account#rentals',
-    '/reservations': '/account#reservations',
-    '/reservations.html': '/account#reservations',
-    '/donations-credits': '/account#donations',
-    '/donations-credits.html': '/account#donations',
-    '/purchases': '/account#purchases',
-    '/purchases.html': '/account#purchases',
-    '/my-membership': '/account#membership',
-    '/my-membership.html': '/account#membership'
-  };
-  
-  const path = window.location.pathname;
-  
-  if (redirectMap[path]) {
-    console.log('📱 [Account] Redirecting to unified account page...');
-    window.location.replace(redirectMap[path]);
-  }
-})();
+
 
 // Membership signup handler
 document.addEventListener('DOMContentLoaded', function() {
